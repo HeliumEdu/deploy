7720b31b0fbba192851dad8d3df935ac0f6f8a41
Alex Laird <alexdlaird@gmail.com>
2018-01-25 17:43:18 -0800

    Bumping copyright year.

--- bin/lib/deploy.py
+++ bin/lib/deploy.py
@@ -4,8 +4,8 @@ import subprocess
 import utils
 
 __author__ = 'Alex Laird'
-__copyright__ = 'Copyright 2017, Helium Edu'
-__version__ = '0.5.0'
+__copyright__ = 'Copyright 2018, Helium Edu'
+__version__ = '1.0.0'
 
 
 class DeployAction:
