9082472185fd9b89f2e930f6226b5aebbb070603
Alex Laird <contact@alexlaird.com>
2024-10-05 15:33:36 -0500

    Terraform cleanup.

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,3 +1,24 @@
+# TODO: provision base bucket (for CI emails) with
+# {
+#    "Version": "2012-10-17",
+#    "Statement": [
+#        {
+#            "Sid": "AllowSESPuts",
+#            "Effect": "Allow",
+#            "Principal": {
+#                "Service": "ses.amazonaws.com"
+#            },
+#            "Action": "s3:PutObject",
+#            "Resource": "arn:aws:s3:::heliumedu-<ENVIRONMENT>/ci.email/*",
+#            "Condition": {
+#                "StringEquals": {
+#                    "aws:Referer": "<ACCOUNT_ID>"
+#                }
+#            }
+#        }
+#    ]
+# }
+#
 # TODO: provision static bucket with policy
 # {
 #    "Version": "2012-10-17",
@@ -12,7 +33,7 @@
 #    ]
 # }
 #
-# TODO: static bucket needs CORS config
+# TODO: provision static bucket with CORS config
 # <?xml version="1.0" encoding="UTF-8"?>
 # <CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
 # <CORSRule>
