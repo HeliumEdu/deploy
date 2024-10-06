62c9b8d5d348cd137a83a6e7f4c6b1fbd45ff25d
Alex Laird <contact@alexlaird.com>
2024-10-05 23:53:49 -0500

    Remaining infra as Terraform committed.

--- terraform/modules/elasticache/variables.tf
+++ terraform/modules/elasticache/variables.tf
@@ -2,3 +2,7 @@ variable "environment" {
   description = "The environment"
   type        = string
 }
+
+variable "elasticache_sg" {
+  description = "The MySQL security group"
+}
--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -2,3 +2,15 @@ variable "environment" {
   description = "The environment"
   type        = string
 }
+
+variable "username" {
+  description = "The DB username"
+}
+
+variable "password" {
+  description = "The DB password"
+}
+
+variable "mysql_sg" {
+  description = "The MySQL security group"
+}
