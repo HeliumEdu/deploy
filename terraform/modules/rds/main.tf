cbd593e07d527b1790b658c35e68b915c33aae17
Alex Laird <contact@alexlaird.com>
2024-10-07 16:11:09 -0500

    Fix identifiers on DB to be env specific.

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -4,6 +4,7 @@ resource "aws_db_subnet_group" "helium" {
 }
 
 resource "aws_db_instance" "helium" {
+  identifier                 = "helium_${var.environment}"
   allocated_storage          = 20
   db_name                    = "platform_${var.environment}"
   engine                     = "mysql"
