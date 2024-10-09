33cac66a8f99f7049775c03ac9317c13bfb1325d
Alex Laird <contact@alexlaird.com>
2024-10-09 14:49:16 -0500

    Bump release to 1.7.5.

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,6 +1,6 @@
 variable "helium_version" {
   description = "The container version. Bumping this will trigger a deploy."
-  default     = "1.7.4"
+  default     = "1.7.5"
 }
 
 variable "environment" {
