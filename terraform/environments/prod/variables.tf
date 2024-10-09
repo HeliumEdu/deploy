534bddb2399603c6a2449591f17cfcc41a3f992a
Alex Laird <contact@alexlaird.com>
2024-10-09 13:18:41 -0500

    Release 1.7.4.

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,6 +1,6 @@
 variable "helium_version" {
-  description = "The container versions to deploy"
-  default     = "1.7.3"
+  description = "The container version. Bumping this will trigger a deploy."
+  default     = "1.7.4"
 }
 
 variable "environment" {
