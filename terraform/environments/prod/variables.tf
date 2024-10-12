fb2336d7cf1bed56a833c6bc2524cc201c3c4337
Alex Laird <contact@alexlaird.com>
2024-10-12 17:10:33 -0500

    Release 1.7.8.

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,6 +1,6 @@
 variable "helium_version" {
   description = "The container version. Bumping this will trigger a deploy."
-  default     = "1.7.7"
+  default     = "1.7.8"
 }
 
 variable "environment" {
