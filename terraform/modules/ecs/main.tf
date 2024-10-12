1dd9c0c921451656d086c0b5f6841b79ff552380
Alex Laird <contact@alexlaird.com>
2024-10-12 17:33:19 -0500

    Platform nodes too small, maxing CPU. Increase size.

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -222,8 +222,8 @@ resource "aws_ecs_task_definition" "platform_service" {
     }
   ])
 
-  cpu    = "512"
-  memory = "1024"
+  cpu    = "1024"
+  memory = "2048"
 
   task_role_arn      = aws_iam_role.ecs_role.arn
   execution_role_arn = aws_iam_role.ecs_role.arn
