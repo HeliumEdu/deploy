6f69825fad9b1db0f17cd2bf39d8be3c154eadfd
Alex Laird <contact@alexlaird.com>
2025-05-22 12:24:19 -0500

    Update main.tf

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,5 +1,5 @@
 locals {
-  about_text = "Helium is an online student planner that lets you organize and color-coordinate your schedule and schoolwork, capture details about every assignment, and plan your study time efficiently."
+  about_text = "Helium is an online student planner that lets you organize and color-coordinate your schedule and schoolwork, capture details about every assignment, and plan your study time efficiently. Check it out at https://www.heliumedu.com."
   platform_usage_text = "Helium is open source, and usage details for this image be found at https://github.com/heliumedu/platform."
 }
 
@@ -10,7 +10,7 @@ resource "aws_ecrpublic_repository" "repository_helium_frontend" {
     operating_systems = ["Linux"]
     architectures = ["x86-64"]
     logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "Frontend images for https://www.heliumedu.com"
+    description = "Images for the frontend's containers."
     about_text = local.about_text
     usage_text = "HeliumEdu is open source, and usage details for this image be found at https://github.com/heliumedu/frontend."
   }
@@ -28,7 +28,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_resource" {
     operating_systems = ["Linux"]
     architectures = ["x86-64"]
     logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "Base platform images for https://www.heliumedu.com"
+    description = "Images for the platform's resource containers (short-lived data provisioning)."
     about_text = local.about_text
     usage_text = local.platform_usage_text
   }
@@ -46,7 +46,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_api" {
     operating_systems = ["Linux"]
     architectures = ["x86-64"]
     logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "API platform images for https://www.heliumedu.com"
+    description = "Images for the platform's API containers."
     about_text = local.about_text
     usage_text = local.platform_usage_text
   }
@@ -64,7 +64,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_worker" {
     operating_systems = ["Linux"]
     architectures = ["x86-64"]
     logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "Worker platform images for https://www.heliumedu.com"
+    description = "Image's for the platform's Worker containers."
     about_text = local.about_text
     usage_text = local.platform_usage_text
   }
