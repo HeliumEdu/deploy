46068d4e5297ad50a5002abe1faf875dafd64ec5
Alex Laird <contact@alexlaird.com>
2025-05-22 10:02:29 -0500

    Update metadata.

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,15 +1,63 @@
 resource "aws_ecrpublic_repository" "repository_helium_frontend" {
   repository_name = "helium/frontend"
+
+  catalog_data {
+    architectures = ["ARM 64"]
+    description = "Frontend images for https://www.heliumedu.com"
+    logo_image_blob = filebase64("https://www.heliumedu.com/assets/img/logo_full_blue.png")
+    operating_systems = ["Linux"]
+  }
+
+  tags = {
+    Service   = "HeliumEdu"
+    Terraform = true
+  }
 }
 
 resource "aws_ecrpublic_repository" "repository_helium_platform_resource" {
   repository_name = "helium/platform-resource"
+
+  catalog_data {
+    architectures = ["ARM 64"]
+    description = "Base platform images for https://www.heliumedu.com"
+    logo_image_blob = filebase64("https://www.heliumedu.com/assets/img/logo_full_blue.png")
+    operating_systems = ["Linux"]
+  }
+
+  tags = {
+    Service   = "HeliumEdu"
+    Terraform = true
+  }
 }
 
 resource "aws_ecrpublic_repository" "repository_helium_platform_api" {
   repository_name = "helium/platform-api"
+
+  catalog_data {
+    architectures = ["ARM 64"]
+    description = "API platform images for https://www.heliumedu.com"
+    logo_image_blob = filebase64("https://www.heliumedu.com/assets/img/logo_full_blue.png")
+    operating_systems = ["Linux"]
+  }
+
+  tags = {
+    Service   = "HeliumEdu"
+    Terraform = true
+  }
 }
 
 resource "aws_ecrpublic_repository" "repository_helium_platform_worker" {
   repository_name = "helium/platform-worker"
+
+  catalog_data {
+    architectures = ["ARM 64"]
+    description = "Worker platform images for https://www.heliumedu.com"
+    logo_image_blob = filebase64("https://www.heliumedu.com/assets/img/logo_full_blue.png")
+    operating_systems = ["Linux"]
+  }
+
+  tags = {
+    Service   = "HeliumEdu"
+    Terraform = true
+  }
 }
