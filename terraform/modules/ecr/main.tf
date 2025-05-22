8475383c3c50a5e745ca119a87708c9d9884cf66
Alex Laird <contact@alexlaird.com>
2025-05-22 10:54:46 -0500

    Switch back to AMD builds for now, until GitHub adds Chrome and driver to ARM runners.

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,7 +2,7 @@ resource "aws_ecrpublic_repository" "repository_helium_frontend" {
   repository_name = "helium/frontend"
 
   catalog_data {
-    architectures = ["ARM 64"]
+    architectures = ["AMD 64"]
     description = "Frontend images for https://www.heliumedu.com"
     logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
@@ -18,7 +18,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_resource" {
   repository_name = "helium/platform-resource"
 
   catalog_data {
-    architectures = ["ARM 64"]
+    architectures = ["AMD 64"]
     description = "Base platform images for https://www.heliumedu.com"
     logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
@@ -34,7 +34,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_api" {
   repository_name = "helium/platform-api"
 
   catalog_data {
-    architectures = ["ARM 64"]
+    architectures = ["AMD 64"]
     description = "API platform images for https://www.heliumedu.com"
     logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
@@ -50,7 +50,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_worker" {
   repository_name = "helium/platform-worker"
 
   catalog_data {
-    architectures = ["ARM 64"]
+    architectures = ["AMD 64"]
     description = "Worker platform images for https://www.heliumedu.com"
     logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
