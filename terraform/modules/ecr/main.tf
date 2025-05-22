99e0ee73162f6061549f6e122d578a137d93a504
Alex Laird <contact@alexlaird.com>
2025-05-22 10:26:04 -0500

    Update main.tf

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,14 +1,9 @@
-data "http" "logo_img" {
-  url = "https://www.heliumedu.com/assets/img/logo_full_blue.png"
-}
-
 resource "aws_ecrpublic_repository" "repository_helium_frontend" {
   repository_name = "helium/frontend"
 
   catalog_data {
     architectures = ["ARM 64"]
     description = "Frontend images for https://www.heliumedu.com"
-    logo_image_blob = base64encode(data.http.logo_img.response_body)
     operating_systems = ["Linux"]
   }
 
@@ -24,7 +19,6 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_resource" {
   catalog_data {
     architectures = ["ARM 64"]
     description = "Base platform images for https://www.heliumedu.com"
-    logo_image_blob = base64encode(data.http.logo_img.response_body)
     operating_systems = ["Linux"]
   }
 
@@ -40,7 +34,6 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_api" {
   catalog_data {
     architectures = ["ARM 64"]
     description = "API platform images for https://www.heliumedu.com"
-    logo_image_blob = base64encode(data.http.logo_img.response_body)
     operating_systems = ["Linux"]
   }
 
@@ -56,7 +49,6 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_worker" {
   catalog_data {
     architectures = ["ARM 64"]
     description = "Worker platform images for https://www.heliumedu.com"
-    logo_image_blob = base64encode(data.http.logo_img.response_body)
     operating_systems = ["Linux"]
   }
 
