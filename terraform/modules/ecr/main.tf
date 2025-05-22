b70f525480874ae997676a22dfd5d213f4994478
Alex Laird <contact@alexlaird.com>
2025-05-22 10:50:00 -0500

    Update main.tf

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -4,7 +4,7 @@ resource "aws_ecrpublic_repository" "repository_helium_frontend" {
   catalog_data {
     architectures = ["ARM 64"]
     description = "Frontend images for https://www.heliumedu.com"
-    logo_image_blob = filebase64("logo.png")
+    logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
   }
 
@@ -20,7 +20,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_resource" {
   catalog_data {
     architectures = ["ARM 64"]
     description = "Base platform images for https://www.heliumedu.com"
-    logo_image_blob = filebase64("logo.png")
+    logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
   }
 
@@ -36,7 +36,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_api" {
   catalog_data {
     architectures = ["ARM 64"]
     description = "API platform images for https://www.heliumedu.com"
-    logo_image_blob = filebase64("logo.png")
+    logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
   }
 
@@ -52,7 +52,7 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_worker" {
   catalog_data {
     architectures = ["ARM 64"]
     description = "Worker platform images for https://www.heliumedu.com"
-    logo_image_blob = filebase64("logo.png")
+    logo_image_blob = filebase64("${path.module}/logo.png")
     operating_systems = ["Linux"]
   }
 
