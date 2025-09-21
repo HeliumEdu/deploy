6bf52e6caf1f0e03ae63293de41aca6e708d8bd8
Alex Laird <contact@alexlaird.com>
2025-09-21 10:27:05 -0500

    Add blog CNAME record.

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -10,6 +10,14 @@ resource "aws_route53_record" "www_heliumedu_com_cname" {
   records = ["${var.environment_prefix}heliumedu.com"]
 }
 
+resource "aws_route53_record" "www_heliumedu_com_cname" {
+  zone_id = aws_route53_zone.heliumedu_com_zone.id
+  name    = "blog.${var.environment_prefix}heliumedu.com"
+  type    = "CNAME"
+  ttl     = "86400"
+  records = ["domains.tumblr.com"]
+}
+
 resource "aws_route53_zone" "heliumedu_dev_zone" {
   name = "${var.environment_prefix}heliumedu.dev"
 }
