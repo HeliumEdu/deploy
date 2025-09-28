fc4675f82eaec463548d1eb0a91cfe48fe7006ae
Alex Laird <contact@alexlaird.com>
2025-09-28 09:58:40 -0500

    Migrating to new platform/frontend S3 buckets.

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -5,12 +5,42 @@ module "ci_bucket" {
   environment    = var.environment
 }
 
-resource "aws_s3_bucket" "heliumedu_static" {
-  bucket = "heliumedu.${var.environment}.static"
+resource "aws_s3_bucket" "heliumedu_media" {
+  bucket = "heliumedu.${var.environment}.media"
 }
 
-resource "aws_s3_bucket_public_access_block" "heliumedu_static_allow_public" {
-  bucket = aws_s3_bucket.heliumedu_static.id
+resource "aws_s3_bucket_public_access_block" "heliumedu_media_block_public" {
+  bucket = aws_s3_bucket.heliumedu_media.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "heliumedu_static_allow_http_access" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      "arn:aws:s3:::heliumedu.${var.environment}.*.static/**",
+    ]
+
+    actions = [
+      "s3:GetObject"
+    ]
+  }
+}
+
+resource "aws_s3_bucket" "heliumedu_platform_static" {
+  bucket = "heliumedu.${var.environment}.platform.static"
+}
+
+resource "aws_s3_bucket_public_access_block" "heliumedu_platform_static_allow_public" {
+  bucket = aws_s3_bucket.heliumedu_platform_static.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -18,10 +48,66 @@ resource "aws_s3_bucket_public_access_block" "heliumedu_static_allow_public" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "heliumedu_platform_static_allow_http_access" {
+  bucket = aws_s3_bucket.heliumedu_platform_static.id
+  policy = data.aws_iam_policy_document.heliumedu_static_allow_http_access.json
+
+  depends_on = [aws_s3_bucket_public_access_block.heliumedu_platform_static_allow_public]
+}
+
+resource "aws_s3_bucket_cors_configuration" "heliumedu_platform_static" {
+  bucket = aws_s3_bucket.heliumedu_platform_static.id
+
+  cors_rule {
+    allowed_headers = ["Authorization"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+resource "aws_s3_bucket" "heliumedu_frontend_static" {
+  bucket = "heliumedu.${var.environment}.frontend.static"
+}
+
+resource "aws_s3_bucket_public_access_block" "heliumedu_frontend_static_allow_public" {
+  bucket = aws_s3_bucket.heliumedu_frontend_static.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "heliumedu_frontend_static_allow_http_access" {
+  bucket = aws_s3_bucket.heliumedu_frontend_static.id
+  policy = data.aws_iam_policy_document.heliumedu_static_allow_http_access.json
+
+  depends_on = [aws_s3_bucket_public_access_block.heliumedu_frontend_static_allow_public]
+}
+
+resource "aws_s3_bucket_cors_configuration" "heliumedu_frontend_static" {
+  bucket = aws_s3_bucket.heliumedu_frontend_static.id
+
+  cors_rule {
+    allowed_headers = ["Authorization"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
+// TODO: legacy S3 buckets, will be removed when new ones are deployed
+resource "aws_s3_bucket" "heliumedu_static" {
+  bucket = "heliumedu.${var.environment}.static"
+}
+
 data "aws_iam_policy_document" "allow_static_http_access" {
   statement {
     principals {
-      type = "*"
+      type        = "*"
       identifiers = ["*"]
     }
 
@@ -39,7 +125,7 @@ resource "aws_s3_bucket_policy" "allow_static_http_access" {
   bucket = aws_s3_bucket.heliumedu_static.id
   policy = data.aws_iam_policy_document.allow_static_http_access.json
 
-  depends_on = [aws_s3_bucket_public_access_block.heliumedu_static_allow_public]
+  depends_on = [aws_s3_bucket_public_access_block.heliumedu_platform_static_allow_public]
 }
 
 resource "aws_s3_bucket_cors_configuration" "heliumedu_static" {
@@ -49,20 +135,7 @@ resource "aws_s3_bucket_cors_configuration" "heliumedu_static" {
     allowed_headers = ["Authorization"]
     allowed_methods = ["GET"]
     allowed_origins = ["*"]
-    expose_headers = ["ETag"]
+    expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
-}
-
-resource "aws_s3_bucket" "heliumedu_media" {
-  bucket = "heliumedu.${var.environment}.media"
-}
-
-resource "aws_s3_bucket_public_access_block" "heliumedu_media_block_public" {
-  bucket = aws_s3_bucket.heliumedu_media.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
