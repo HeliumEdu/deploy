8bc0826d343dba5c5abecbeaf41ee81b74386d2b
Alex Laird <contact@alexlaird.com>
2025-09-28 17:59:39 -0500

    Remove legacy S3 bucket.

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -51,7 +51,7 @@ resource "aws_lb_target_group" "frontend" {
   vpc_id      = var.helium_vpc_id
 
   health_check {
-    path = "/health"
+    path = "/health.json"
   }
 
   depends_on = [aws_lb.helium]
