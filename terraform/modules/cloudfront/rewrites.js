355a3bdd6752540f0ea8ad85c7e915fab6d23e5a
Alex Laird <contact@alexlaird.com>
2025-09-29 18:17:51 -0500

    Update rewrites.js

--- terraform/modules/cloudfront/rewrites.js
+++ terraform/modules/cloudfront/rewrites.js
@@ -2,14 +2,35 @@ function handler(event) {
     var request = event.request;
     var uri = request.uri;
 
-    if (uri.endsWith('/')) {
-        request.uri += 'index.html';
-    } else if (!uri.includes('.')) {
-        request.uri += '.html';
+    if (uri.endsWith('.html')) {
+        var newUri = uri.slice(0, -5);
+        return {
+            statusCode: 301,
+            statusDescription: 'Moved Permanently',
+            headers: {
+                'location': {
+                    'value': newUri
+                }
+            }
+        };
+    } else if (uri === "/index") {
+        return {
+            statusCode: 301,
+            statusDescription: 'Moved Permanently',
+            headers: {
+                'location': {
+                    'value': "/"
+                }
+            }
+        };
     }
 
-    if (uri === '/info') {
+    if (!uri.includes('.') && !uri.endsWith('/')) {
+        request.uri += '.html';
+    } else if (uri === '/info') {
         request.uri = '/assets/info.json';
+    } else if (uri.endsWith('/')) {
+        request.uri += 'index.html';
     }
 
     return request;
