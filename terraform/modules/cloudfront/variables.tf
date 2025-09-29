0eeae49562906515bbf30010e20eda26cadf8a91
Alex Laird <contact@alexlaird.com>
2025-09-29 12:41:07 -0500

    Refactor environment handling and add SSL to CloudFront

--- terraform/modules/cloudfront/variables.tf
+++ terraform/modules/cloudfront/variables.tf
@@ -1,7 +1,24 @@
+variable "environment" {
+  description = "The environment"
+  type        = string
+}
+
+variable "environment_prefix" {
+  description = "Prefix used for env in hostnames (empty string when `prod`)"
+  type        = string
+}
+
 variable "aws_region" {
   description = "The AWS region"
+  type        = string
+}
+
+variable "heliumedu_com_cert_arn" {
+  description = "The ARN of the <env>heliumedu.com SSL certificate"
+  type        = string
 }
 
 variable "s3_bucket" {
   description = "The S3 bucket to host"
+  type        = string
 }
--- terraform/modules/email/main.tf
+++ terraform/modules/email/main.tf
@@ -1,22 +1,30 @@
+// Records only created once, for production
+
 resource "aws_route53_record" "heliumedu_com_inbound_mx" {
+  count = var.environment == "prod" ? 1 : 0
+
   zone_id = var.route53_heliumedu_com_zone_id
-  name    = "${var.environment_prefix}heliumedu.com"
+  name    = "heliumedu.com"
   type    = "MX"
   ttl     = "3600"
   records = ["10 mx1.privateemail.com", "10 mx2.privateemail.com"]
 }
 
 resource "aws_route53_record" "heliumedu_com_spf" {
+  count = var.environment == "prod" ? 1 : 0
+
   zone_id = var.route53_heliumedu_com_zone_id
-  name    = "${var.environment_prefix}heliumedu.com"
+  name    = "heliumedu.com"
   type    = "TXT"
   ttl     = "3600"
   records = ["v=spf1 include:spf.privateemail.com ~all"]
 }
 
 resource "aws_route53_record" "heliumedu_com_dkim" {
+  count = var.environment == "prod" ? 1 : 0
+
   zone_id = var.route53_heliumedu_com_zone_id
-  name    = "default._domainkey.${var.environment_prefix}heliumedu.com"
+  name    = "default._domainkey.heliumedu.com"
   type    = "TXT"
   ttl     = "3600"
   records = [
