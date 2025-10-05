29e82fc02c7c101bdbeb45d1cee91c88e5e303a3
Alex Laird <contact@alexlaird.com>
2025-10-04 20:59:11 -0500

    Add cloudfront routes for subdomains (ensures SSL is possible).

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -52,14 +52,62 @@ resource "aws_s3_bucket_policy" "heliumedu_support_redirect_allow_http_access" {
   depends_on = [aws_s3_bucket_public_access_block.heliumedu_support_redirect_allow_public]
 }
 
+resource "aws_cloudfront_distribution" "support_heliumedu_com" {
+  enabled             = true
+  aliases             = ["support.${var.environment_prefix}heliumedu.com"]
+  comment             = "support.${var.environment_prefix}heliumedu.com"
+  price_class         = "PriceClass_100"
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.support_redirect_bucket.website_endpoint
+    origin_id   = "${aws_s3_bucket.support_redirect_bucket.bucket}-origin"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id = "${aws_s3_bucket.support_redirect_bucket.bucket}-origin"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    default_ttl            = 0
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = var.heliumedu_com_cert_arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
 resource "aws_route53_record" "support_heliumedu_com" {
   zone_id = aws_route53_zone.heliumedu_com_zone.zone_id
   name    = "support.${var.environment_prefix}heliumedu.com"
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.support_redirect_bucket.website_endpoint
-    zone_id                = aws_s3_bucket.support_redirect_bucket.hosted_zone_id
+    name                   = aws_cloudfront_distribution.support_heliumedu_com.domain_name
+    zone_id                = aws_cloudfront_distribution.support_heliumedu_com.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -110,14 +158,62 @@ resource "aws_s3_bucket_policy" "heliumedu_app_redirect_allow_http_access" {
   depends_on = [aws_s3_bucket_public_access_block.heliumedu_app_redirect_allow_public]
 }
 
+resource "aws_cloudfront_distribution" "app_heliumedu_com" {
+  enabled             = true
+  aliases             = ["app.${var.environment_prefix}heliumedu.com"]
+  comment             = "app.${var.environment_prefix}heliumedu.com"
+  price_class         = "PriceClass_100"
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.app_redirect_bucket.website_endpoint
+    origin_id   = "${aws_s3_bucket.app_redirect_bucket.bucket}-origin"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id = "${aws_s3_bucket.app_redirect_bucket.bucket}-origin"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    default_ttl            = 0
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = var.heliumedu_com_cert_arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
 resource "aws_route53_record" "app_heliumedu_com" {
   zone_id = aws_route53_zone.heliumedu_com_zone.zone_id
   name    = "app.${var.environment_prefix}heliumedu.com"
   type    = "A"
 
   alias {
-    name                   = aws_s3_bucket_website_configuration.app_redirect_bucket.website_endpoint
-    zone_id                = aws_s3_bucket.app_redirect_bucket.hosted_zone_id
+    name                   = aws_cloudfront_distribution.app_heliumedu_com.domain_name
+    zone_id                = aws_cloudfront_distribution.app_heliumedu_com.hosted_zone_id
     evaluate_target_health = false
   }
 }
--- terraform/modules/route53/variables.tf
+++ terraform/modules/route53/variables.tf
@@ -7,3 +7,8 @@ variable "environment_prefix" {
   description = "Prefix used for env in hostnames (empty string when `prod`)"
   type        = string
 }
+
+variable "heliumedu_com_cert_arn" {
+  description = "The ARN of the <env>heliumedu.com SSL certificate"
+  type        = string
+}
