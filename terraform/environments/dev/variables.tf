917b6f63bb0022a16c54695ea4a2b6c25578c616
Alex Laird <contact@alexlaird.com>
2025-10-23 16:38:09 -0500

    Point to latest container images for dev.

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,6 +1,6 @@
 variable "helium_version" {
   description = "The container version. Bumping this will trigger a deploy."
-  default     = "1.14.3"
+  default     = "latest"
 }
 
 variable "environment" {
