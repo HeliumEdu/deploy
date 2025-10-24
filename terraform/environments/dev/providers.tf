a5858645947ff450896149f662951b4127aaae0f
Alex Laird <contact@alexlaird.com>
2025-10-24 13:18:02 -0500

    Pass provider to ALB module.

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -8,3 +8,15 @@ provider "aws" {
     }
   }
 }
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "us_east_1"
+
+  default_tags {
+    tags = {
+      Environment = var.environment
+      Terraform   = true
+    }
+  }
+}
