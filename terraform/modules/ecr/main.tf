fe80d13e8fa480b21f7c5d4a9df8bc5fec1d8ed0
Alex Laird <contact@alexlaird.com>
2025-10-24 14:19:21 -0500

    Add expiration policy for untagged images.

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,5 +1,5 @@
 locals {
-  about_text = "Helium is an online student planner that lets you organize and color-coordinate your schedule and schoolwork, capture details about every assignment, and plan your study time efficiently. Check it out at https://www.heliumedu.com."
+  about_text          = "Helium is an online student planner that lets you organize and color-coordinate your schedule and schoolwork, capture details about every assignment, and plan your study time efficiently. Check it out at https://www.heliumedu.com."
   platform_usage_text = "Helium is open source, and usage details for this image be found at https://github.com/heliumedu/platform."
 }
 
@@ -8,11 +8,11 @@ resource "aws_ecrpublic_repository" "repository_helium_frontend" {
 
   catalog_data {
     operating_systems = ["Linux"]
-    architectures = ["x86-64"]
-    logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "Images for the frontend's containers."
-    about_text = local.about_text
-    usage_text = "HeliumEdu is open source, and usage details for this image be found at https://github.com/heliumedu/frontend."
+    architectures     = ["x86-64"]
+    logo_image_blob   = filebase64("${path.module}/../../resources/logo.png")
+    description       = "Images for the frontend's containers."
+    about_text        = local.about_text
+    usage_text        = "HeliumEdu is open source, and usage details for this image be found at https://github.com/heliumedu/frontend."
   }
 
   tags = {
@@ -21,22 +21,64 @@ resource "aws_ecrpublic_repository" "repository_helium_frontend" {
   }
 }
 
+resource "aws_ecr_lifecycle_policy" "frontend_untagged_expiration_policy" {
+  repository = aws_ecrpublic_repository.repository_helium_frontend
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Expire untagged images",
+        selection = {
+          tagStatus   = "untagged",
+          countType   = "sinceImagePushed",
+          countUnit   = "days",
+          countNumber = 1
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_ecrpublic_repository" "repository_helium_platform_resource" {
   repository_name = "helium/platform-resource"
 
   catalog_data {
     operating_systems = ["Linux"]
-    architectures = ["x86-64"]
-    logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "Images for the platform's resource containers (short-lived data provisioning)."
-    about_text = local.about_text
-    usage_text = local.platform_usage_text
+    architectures     = ["x86-64"]
+    logo_image_blob   = filebase64("${path.module}/../../resources/logo.png")
+    description       = "Images for the platform's resource containers (short-lived data provisioning)."
+    about_text        = local.about_text
+    usage_text        = local.platform_usage_text
   }
 
   tags = {
     Environment = "N/A"
-    Service   = "HeliumEdu"
+    Service     = "HeliumEdu"
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "resource_untagged_expiration_policy" {
+  repository = aws_ecrpublic_repository.repository_helium_platform_resource
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Expire untagged images",
+        selection = {
+          tagStatus   = "untagged",
+          countType   = "sinceImagePushed",
+          countUnit   = "days",
+          countNumber = 1
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_ecrpublic_repository" "repository_helium_platform_api" {
@@ -44,17 +86,38 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_api" {
 
   catalog_data {
     operating_systems = ["Linux"]
-    architectures = ["x86-64"]
-    logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "Images for the platform's API containers."
-    about_text = local.about_text
-    usage_text = local.platform_usage_text
+    architectures     = ["x86-64"]
+    logo_image_blob   = filebase64("${path.module}/../../resources/logo.png")
+    description       = "Images for the platform's API containers."
+    about_text        = local.about_text
+    usage_text        = local.platform_usage_text
   }
 
   tags = {
     Environment = "N/A"
-    Service   = "HeliumEdu"
+    Service     = "HeliumEdu"
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "api_untagged_expiration_policy" {
+  repository = aws_ecrpublic_repository.repository_helium_platform_api
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Expire untagged images",
+        selection = {
+          tagStatus   = "untagged",
+          countType   = "sinceImagePushed",
+          countUnit   = "days",
+          countNumber = 1
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_ecrpublic_repository" "repository_helium_platform_worker" {
@@ -62,15 +125,36 @@ resource "aws_ecrpublic_repository" "repository_helium_platform_worker" {
 
   catalog_data {
     operating_systems = ["Linux"]
-    architectures = ["x86-64"]
-    logo_image_blob = filebase64("${path.module}/../../resources/logo.png")
-    description = "Image's for the platform's Worker containers."
-    about_text = local.about_text
-    usage_text = local.platform_usage_text
+    architectures     = ["x86-64"]
+    logo_image_blob   = filebase64("${path.module}/../../resources/logo.png")
+    description       = "Image's for the platform's Worker containers."
+    about_text        = local.about_text
+    usage_text        = local.platform_usage_text
   }
 
   tags = {
     Environment = "N/A"
-    Service   = "HeliumEdu"
+    Service     = "HeliumEdu"
   }
+}
+
+resource "aws_ecr_lifecycle_policy" "worker_untagged_expiration_policy" {
+  repository = aws_ecrpublic_repository.repository_helium_platform_worker
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Expire untagged images",
+        selection = {
+          tagStatus   = "untagged",
+          countType   = "sinceImagePushed",
+          countUnit   = "days",
+          countNumber = 1
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
