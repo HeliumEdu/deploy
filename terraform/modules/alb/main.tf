21a4ac4fa51f3e8600fe2282547cdbd477fdd4f9
Alex Laird <contact@alexlaird.com>
2025-10-24 13:15:24 -0500

    Force LBs to US1 for CloudFront.

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_lb" "helium" {
   name               = "helium-${var.environment}"
   internal           = false
   load_balancer_type = "application"
-  security_groups = [var.security_group]
+  security_groups    = [var.security_group]
   subnets            = [for id in var.subnet_ids : id]
 
   enable_deletion_protection = true
