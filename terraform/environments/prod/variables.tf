57bdb682a85b7c2a21dbafc64879dbe5e6419da1
Alex Laird <contact@alexlaird.com>
2026-02-09 19:11:11 -0600

    [platform] Deploy 1.18.12 to prod

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,6 +1,6 @@
 variable "helium_version" {
   description = "The container version. Bumping this will trigger a deploy."
-  default     = "1.18.11"
+  default     = "1.18.12"
 }
 
 variable "environment" {
